c97f2f51845d8d106f419f2f8e0bbe30573deda0
abigail <abigailsegovia1309@gmail.com>
2025-06-15 19:46:33 -0600

    Agregué una suma

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert ('Hola mundo')
+//alert ('Hola mundo')
+let a = 1;
+let b = 3;
+let resultado = 0;
+
+resultado = a + b;
+console.log ("La suma es:" + resultado);
+alert ("La suma es: " + resultado);
